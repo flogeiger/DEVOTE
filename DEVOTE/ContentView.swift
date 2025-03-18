208328ee34342278212e298e8b5d5117aad04a43
fgeiger <florian.geiger21@gmail.com>
2025-03-18 22:16:38 +0100

    Adjusted Core Data Model and created UI Change

--- DEVOTE/ContentView.swift
+++ DEVOTE/ContentView.swift
@@ -10,34 +10,25 @@ import CoreData
 
 struct ContentView: View {
     // MARK: - PROPERTY
-    @Environment(\.managedObjectContext) private var viewContext
-
+    
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
+    @State private var task: String = ""
+    @State private var showNewTaskItem: Bool = false
+    
     // FETCHING DATA
+    @Environment(\.managedObjectContext) private var viewContext
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        animation: .default
+    ) private var items: FetchedResults<Item>
     
     // MARK: - FUNCTION
     
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -47,33 +38,102 @@ struct ContentView: View {
         }
     }
     
+    // MARK: - BODY
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            ZStack {
+                VStack {
+                    // MARK: - HEADER
+                    HStack(spacing: 10) {
+                        Text("Devote")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
+                        
+                        Spacer()
+                        
+                        EditButton()
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(
+                                Capsule().stroke(Color.white, lineWidth: 2)
+                            )
+                        
+                        Button(action: {
+                            isDarkMode.toggle()
+                        }, label: {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" :  "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        })
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .padding()
+                    .foregroundColor(.white)
+                    
+                    Spacer(minLength: 80)
+                    
+                    // MARK: - NEW TASK BUTTON
+                    Button(action: {
+                        showNewTaskItem = true
+                    }, label: {
+                        HStack {
+                            Image(systemName: "plus.circle")
+                                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                            Text("New Task")
+                                .font(.system(size: 24, weight: .bold, design: .rounded))
+                        }
+                    })
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                            .clipShape(Capsule())
+                    )
+                    .shadow(color: Color.black.opacity(0.25), radius: 8, x: 0.0, y: 4.0)
+                    
+                    // MARK: - TASKS
+                    List {
+                        ForEach(items) { item in
+                            Toggle(isOn: Binding(
+                                get: { item.completion ?? false },
+                                set: { newValue in item.completion = newValue }
+                            )) {
+                                Text(item.task ?? "")
+                                    .font(.system(.title2, design: .rounded))
+                                    .fontWeight(.heavy)
+                                    .foregroundColor((item.completion ?? false) ? Color.pink : Color.primary)
+                                    .padding(.vertical, 12)
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
                     }
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color.black.opacity(0.3), radius: 12)
+                    .frame(maxWidth: 640)
                 }
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .animation(.easeOut(duration: 0.5), value: showNewTaskItem)
             }
-            Text("Select an item")
+            .onAppear {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            }
+            .navigationBarTitle("Daily Tasks", displayMode: .large)
+            .navigationBarHidden(true)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+            )
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
--- DEVOTE/Utility/Constants.swift
+++ DEVOTE/Utility/Constants.swift
@@ -6,9 +6,21 @@
 //
 import SwiftUI
 
+// MARK: - FORMATTER
+
 let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
 }()
+
+// MARK: - UI
+
+var backgroundGradient: LinearGradient {
+  return LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+}
+
+// MARK: - UX
+
+let feedback = UINotificationFeedbackGenerator()
