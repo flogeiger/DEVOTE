b090de3edcfd1b5d7505126fa6edbfc4d6331113
fgeiger <florian.geiger21@gmail.com>
2025-03-27 22:32:33 +0100

    embedded Widget View

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Florian Geiger on 26.03.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
